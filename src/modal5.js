dab26821ef17fa7dc9d94ec1b88d0145795d0816
Wojciech Maroszek <wmaroszek1@wp.pl>
2023-04-06 17:59:07 +0200

    fix: modal

---
+++ src/modal5.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open5]'),
+    closeModalBtn: document.querySelector('[data-modal-close5]'),
+    modal: document.querySelector('[data-modal5]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
